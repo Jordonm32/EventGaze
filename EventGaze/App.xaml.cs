1327ee8cf48e25467407c353305d0c193b5739db
Jordonm32 <Jordonm32@github.com>
2024-08-08 18:58:06 -0400

    CreateBase Project

---
+++ EventGaze/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EventGaze
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EventGaze/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EventGaze
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
